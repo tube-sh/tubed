e7f212c62faffd6ef93272e3d9c93495cdb35c8d
Romain Grente <romain.grente@gmail.com>
2022-04-18 20:51:26 +0000

    daemon init

---
+++ main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"flag"
+	"github.com/sevlyar/go-daemon"
+	"log"
+	"os"
+	"syscall"
+	"time"
+	"os/exec"
+)
+
+var pid int
+
+var (
+	signal = flag.String("s", "", `Send signal to the daemon:
+  quit — graceful shutdown
+  stop — fast shutdown
+  reload — reloading the configuration file`)
+)
+
+func main() {
+	flag.Parse()
+	daemon.AddCommand(daemon.StringFlag(signal, "quit"), syscall.SIGQUIT, termHandler)
+	daemon.AddCommand(daemon.StringFlag(signal, "stop"), syscall.SIGTERM, termHandler)
+	daemon.AddCommand(daemon.StringFlag(signal, "reload"), syscall.SIGHUP, reloadHandler)
+
+	cntxt := &daemon.Context{
+		PidFileName: "sample.pid",
+		PidFilePerm: 0644,
+		LogFileName: "sample.log",
+		LogFilePerm: 0640,
+		WorkDir:     "./",
+		Umask:       027,
+		Args:        []string{"[go-daemon sample]"},
+	}
+
+	if len(daemon.ActiveFlags()) > 0 {
+		d, err := cntxt.Search()
+		if err != nil {
+			log.Fatalf("Unable send signal to the daemon: %s", err.Error())
+		}
+		daemon.SendCommands(d)
+		return
+	}
+
+	d, err := cntxt.Reborn()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if d != nil {
+		return
+	}
+	defer cntxt.Release()
+
+	log.Println("- - - - - - - - - - - - - - -")
+	log.Println("daemon started")
+
+	go worker()
+
+	err = daemon.ServeSignals()
+	if err != nil {
+		log.Printf("Error: %s", err.Error())
+	}
+
+	log.Println("daemon terminated")
+}
+
+var (
+	stop = make(chan struct{})
+	done = make(chan struct{})
+)
+
+func worker() {
+	cmd := exec.Command("./test.sh")
+	//cmd.Stdout = os.Stdout
+	err := cmd.Start()
+	if err != nil {
+	   log.Fatal(err)
+	}
+	pid = cmd.Process.Pid
+	log.Printf("frpc subprocess pid is %d\n", cmd.Process.Pid)
+	
+	LOOP:
+	for {
+		// check if frpc pid ok (api call + pid)
+		// get tunnel updates
+		//log.Printf("Just ran subprocess %d, exiting\n", cmd.Process.Pid)
+		time.Sleep(time.Second) // this is work to be done by worker.
+		//log.Println(process.PidExists(pid))
+		select {
+		case <-stop:
+			break LOOP
+		default:
+		}
+	}
+	done <- struct{}{}
+}
+
+func termHandler(sig os.Signal) error {
+	err := syscall.Kill(pid, 9)
+	if err == nil {
+		log.Println("frpc subprocess stopped")
+	}
+	stop <- struct{}{}
+	if sig == syscall.SIGQUIT {
+		<-done
+	}
+	return daemon.ErrStop
+}
+
+func reloadHandler(sig os.Signal) error {
+	log.Println("configuration reloaded")
+	return nil
+}
---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+while true; do
+    echo "ok"
+    sleep 2
+done
