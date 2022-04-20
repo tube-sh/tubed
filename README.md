c92f6e2f85338b6076293016be96aacff34d97e2
Romain Grente <romain.grente@gmail.com>
2022-04-20 20:33:06 +0000

    install.sh script + Readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# tubed
+
+## Installation
+```bash
+sudo su -c "bash <(wget -qO- https://raw.githubusercontent.com/tube-sh/tubed/main/install.sh <TOKEN>)" root
+```
--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@
 
 # VARIABLES
 TUBED_DIR="/etc/tubed"
-TUBED_USER="tubed"
+#TUBED_USER="tubed"
 TUBED_TOKEN=${1}
 #TUBED_SERVICE="tubed.service"
 
 # CHECK IF RUN AS ROOT
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 
+   echo "script must be run as root" 
    exit 1
 fi
 
@@ -16,20 +16,28 @@ fi
 #useradd -r -s /bin/false ${TUBED_USER}
 
 # CREATE TUBED DIR
+echo "create ${TUBED_DIR} directory"
 mkdir -p ${TUBED_DIR}
 
 # CREATE TOKEN FILE
+echo "write token in ${TUBED_DIR}/token file"
 echo ${TUBED_TOKEN} > ${TUBED_DIR}/token
+
+echo "set rights to ${TUBED_DIR}/token file"
 chmod 400 ${TUBED_DIR}/token
 
 # CHANGE DIRECTORY/FILES OWNER
 #chown -R ${TUBED_USER}:${TUBED_USER} ${TUBED_DIR}
 
 # GET TUBED BINARY
+echo "get tubed archive and extract it"
+wget -qO- https://github.com/tube-sh/tubed/releases/download/v0.0.1-alpha/tubed_v0.0.1-alpha_linux_amd64.tar.gz | tar -xvz -C /usr/bin/
+
 # INSTALL TUBED BINARY
-cp tubed /usr/bin/tubed
-chmod 755 /usr/bin/tubed
+#cp tubed /usr/bin/tubed
+#chmod 755 /usr/bin/tubed
 
 # INSTALL SERVICE FILE
 #cp tubed.service /etc/systemd/system/
 #chmod 664 /etc/systemd/system/tubed.service
+
